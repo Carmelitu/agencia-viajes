1498e3a252372b09539ba47448d2ffb32a435b84
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-20 10:02:39 -0300

    habilitando express y router

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
-const express = require('express');
+import express from 'express';
+import router from './routes/index.js';
+
 
 const app = express();
 
 // Definir Puerto
-
 const port = process.env.PORT || 4000;
+
+// Agregar Router
+app.use('/', router);
+
+
+
 
 app.listen(port, () => {
     console.log(`Servidor Funcionando en el puerto ${port}`);
