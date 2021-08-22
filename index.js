be026beb4b8ed3e6b63bd20452fa1daf6d51cef2
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-22 18:31:08 -0300

    base de datos creada y conectada

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 import express from 'express';
 import router from './routes/index.js';
+import db from './config/db.js';
 
 
 const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+    .then( () => console.log('Base de datos conectada'))
+    .catch( error => console.log(error))
+
 
 // Definir Puerto
 const port = process.env.PORT || 4000;
