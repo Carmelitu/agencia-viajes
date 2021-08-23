329157249efee2694caa885d32c74f8d97a8559c
Lucas Seijo <lucaseijo7@gmail.com>
2021-08-23 11:21:33 -0300

    preparando deploy para heroku

--- config/db.js
+++ config/db.js
@@ -1,8 +1,10 @@
 import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
 
-const db = new Sequelize('agenciadeviajes', 'root', '', {
-    host: '127.0.0.1', 
-    port: '3306',
+const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
+    host: process.env.BD_HOST, 
+    port: process.env.BD_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import router from './routes/index.js';
 import db from './config/db.js';
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
 
 
 const app = express();
@@ -9,10 +11,6 @@ const app = express();
 db.authenticate()
     .then( () => console.log('Base de datos conectada'))
     .catch( error => console.log(error))
-
-
-// Definir Puerto
-const port = process.env.PORT || 4000;
 
 // Habilitar PUG
 app.set('view engine', 'pug');
@@ -35,6 +33,10 @@ app.use(express.static('public'));
 // Agregar Router
 app.use('/', router);
 
-app.listen(port, () => {
+// Puerto y Host para la app
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
     console.log(`Servidor Funcionando en el puerto ${port}`);
 });
